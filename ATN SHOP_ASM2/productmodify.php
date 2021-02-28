598b43ea8e1adaf246f68581c14b3d799f04da4d
vanminh.le <vanminh.le.754@gmail.com>
2021-02-28 22:14:56 +0700

    Update productmodify.php

--- ATN SHOP_ASM2/productmodify.php
+++ ATN SHOP_ASM2/productmodify.php
@@ -57,7 +57,7 @@ $resultCheck = pg_num_rows($result);
                   <?php echo $row['product_name']; ?>
                 </td>
                 <td>
-                  <?php echo $row['product_price']; Đ?>
+                  <?php echo $row['product_price']; ?>
                 </td>
                 <td>
                   <?php echo $row['product_category']; ?>
