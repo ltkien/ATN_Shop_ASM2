fbce032ce2e6aceaca98d078b7eb12182f4e4007
vanminh.le <vanminh.le.754@gmail.com>
2021-02-28 15:29:38 +0700

    Update productdelete.php

--- ATN SHOP_ASM2/productdelete.php
+++ ATN SHOP_ASM2/productdelete.php
@@ -1,11 +1,11 @@
 <?php
+echo ("ERROR");
 $connect = pg_connect("host=ec2-54-220-35-19.eu-west-1.compute.amazonaws.com
 dbname=d3d86pg4upllsh
 port=5432
 user=zmvsrcbksqwkdt
 password=9c571947fb86d6d6fbde944f4bd9d3dfd522e56b3652b3731394cd32983ef33e
 sslmode=require");
-echo ("ERROR");
 if ($connect === false) {
   die("ERROR: Could not connect to the database server!");
 } else {
